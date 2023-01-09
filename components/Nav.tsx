24ca1b5aaced6b0379ab92b2c49e11af182c8dcf
Ishaan Bedi <bediishaan6@gmail.com>
2023-01-09 14:14:11 +0530

    Minor Changes

--- components/Nav.tsx
+++
@@ -1,9 +0,0 @@
-const Nav = () => {
-  return (
-    <div className="py-4 flex justify-evenly">
-      <h1>Webnotes</h1>
-    </div>
-  );
-};
-
-export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,80 @@
 import type { NextPage } from "next";
-import Nav from "../components/Nav";
-import Writing from "../components/Writing";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 const Home: NextPage = () => {
+  const [hideButton, setHideButton] = useState(false);
+  const [text, setText] = useState("");
+  useEffect(() => {
+    if (localStorage.getItem("text") === null) {
+      setHideButton(true);
+    } else {
+      setHideButton(false);
+    }
+  }, [text]);
+  useEffect(() => {
+    if (localStorage.getItem("text") === null) {
+      setText("");
+    } else {
+      setText(JSON.parse(localStorage.getItem("text") || ""));
+    }
+  }, []);
+  const handleTextChange = (value: string) => {
+    localStorage.setItem("text", JSON.stringify(value));
+    setText(value);
+  };
+  const downloadTextAsFile = (text: string) => {
+    const element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+    );
+    element.setAttribute("download", "webnote.txt");
+    element.style.display = "none";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
+  const clearTextArea = () => {
+    localStorage.removeItem("text");
+    setText("");
+    setHideButton(true);
+  };
   return (
     <div>
+      <Head>
+        <title>webnotes | take quick notes</title>
+      </Head>
       <header>
-        <Nav />
-        <Writing />
+        <div className="py-4 flex justify-between px-5 bg-gray-50 sticky w-screen">
+          <h1 className="text-xl font-bold">webnotes</h1>
+          <ul className="flex space-x-3">
+            <span
+              className={`${hideButton ? "hidden" : "block"} flex space-x-3`}
+            >
+              <li
+                onClick={() => {
+                  downloadTextAsFile(text);
+                }}
+              >
+                <button>Download</button>
+              </li>
+              <li onClick={clearTextArea}>
+                <button>Clear</button>
+              </li>
+            </span>
+          </ul>
+        </div>
       </header>
+      <section>
+        <textarea
+          id="textarea"
+          className="h-screen w-screen px-5 pt-3 outline-none resize-none	"
+          placeholder="Start typing here..."
+          value={text}
+          onChange={(e) => handleTextChange(e.target.value)}
+        />
+      </section>
     </div>
   );
 };
