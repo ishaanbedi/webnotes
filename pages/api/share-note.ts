dc9d9a69930aa120dc5e39edb2dcc77f2cc5f847
Ishaan Bedi <bediishaan6@gmail.com>
2023-01-10 09:08:47 +0530

    Remove API Key Verification [Not Needed]

--- pages/api/share-note.ts
+++ pages/api/share-note.ts
@@ -9,11 +9,6 @@ var handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(403).json("Forbidden");
     return;
   }
-  var apiKeyServer = process.env.API_KEY_SERVER;
-  if (req.query.apiKey !== apiKeyServer) {
-    res.status(403).json("Forbidden");
-    return;
-  }
   const record = await xata.db.shared_webnotes.create({
     note: JSON.stringify(req.query.note),
   });
